2e187d98de28f8e5420e58fae59e32de1e1f40ce
Mustapha Rashiduddin <mustapha.rashiduddin@gmail.com>
2016-02-07 19:36:31 +0500

    added ycm config file for c99

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include "sayf_debug/sayf_memory.h"
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    int* pNum = MALLOC(sizeof(int), 2);
+    int *pNum = (int *)MALLOC(sizeof(int), 2);
     /*printf("%p---\n", pNum);*/
-    char* l = MALLOC(sizeof(char), 3);
+    char *l = (char *)MALLOC(sizeof(char), 3);
     /*printf("%p---\n", l);*/
     *pNum = 3;
     free(pNum);
+    free(l);
     return 0;
 }
